65c674acd8510675e26c5b4ac5da5fdd344d5d41
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-08-14 08:16:40 +0300

    Added jQuery support

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 function weather() {
 
-    let location = document.getElementById("location");
     let apiKey = "7d4a8b76ab3a113b2bb79af067b79eeb";
     let url = "https://api.forecast.io/forecast/";
 
@@ -8,6 +7,7 @@ function weather() {
 
     let temperature = document.getElementById("temperature");
     let minutely = document.getElementById("minutely");
+    let location = document.getElementById("location");
 
     navigator.geolocation.getCurrentPosition(showPosition);
 
@@ -19,32 +19,37 @@ function weather() {
 							 "longitude is " + longitude + "°";
 
         function generateURL() {
-            generated_url ="https://cors-escape.herokuapp.com/" + url + apiKey + "/" +
+            generated_url = url + apiKey + "/" +
                             latitude + "," + longitude + "?callback=?";
             return generated_url;
         }
         generateURL();
         console.log(generated_url);
 
-        let request = new XMLHttpRequest();
-        request.open('GET', generated_url, true);
+        // let request = new XMLHttpRequest();
+        // request.open('GET', generated_url, true);
 
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 400) {
-                // Success!
-                let data = JSON.parse(request.responseText);
-                temperature.innerHTML = data.currently.temperature + "° F";
-                minutely.innerHTML = data.minutely.summary;
-            } else {
-                error();
-            }
-        };
+        // request.onload = function() {
+        //     if (request.status >= 200 && request.status < 400) {
+        //         // Success!
+        //         let data = JSON.parse(request.responseText);
+        //         temperature.innerHTML = data.currently.temperature + "° F";
+        //         minutely.innerHTML = data.minutely.summary;
+        //     } else {
+        //         error();
+        //     }
+        // };
 
-        request.onerror = function() {
-            error();
-        };
+        // request.onerror = function() {
+        //     error();
+        // };
 
-        request.send();
+        // request.send();
+
+        $.getJSON(generated_url, function(data) {
+        	$("#temperature").html(data.currently.temperature + "° F");
+        	$("#minutely").html(data.minutely.summary);
+        });
 
         function error() {
             location.innerHTML = "Unable to retrieve your location";
